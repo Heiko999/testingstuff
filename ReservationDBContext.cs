a1d0f4150991d051035eaa3118f93f033a54aa0b
Heiko999 <84904473+Heiko999@users.noreply.github.com>
2022-12-29 20:04:15 +0100

    Update ReservationDBContext.cs

--- ReservationDBContext.cs
+++ ReservationDBContext.cs
@@ -1,8 +1,14 @@
-public class ReservationDbContext : DbContext
+using Microsoft.EntityFrameworkCore;
+
+namespace Biletrado Reservations 
 {
+    public class ReservationDbContext : DbContext
+    {
     public DbSet<Reservation> Reservations { get; set; }
 
-    public MyDbContext(DbContextOptions<MyDbContext> options)
-        : base(options)
-    { }
+        public MyDbContext(DbContextOptions<ReservationDbContext> options)
+             : base(options)
+        { 
+        }
+    }
 }
